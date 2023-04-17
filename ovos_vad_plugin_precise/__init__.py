c58f7c9b9a0a4d7c0a3ecc81743966d627084f2b
JarbasAi <jarbasai@mailfence.com>
2023-04-17 19:35:05 +0100

    add debug flag + readme

--- ovos_vad_plugin_precise/__init__.py
+++ ovos_vad_plugin_precise/__init__.py
@@ -1,5 +1,5 @@
 from os.path import join, dirname
-
+from ovos_utils.log import LOG
 from ovos_plugin_manager.templates.vad import VADEngine
 from precise_lite_runner.runner import Listener
 
@@ -9,9 +9,12 @@ class PreciseVAD(VADEngine):
         super().__init__(config, sample_rate)
         model = self.config.get("model") or join(dirname(__file__), "vad.tflite")
         self.vad_threshold = self.config.get("threshold", 0.5)
+        self.debug = self.config.get("debug", False)
         self.vad = Listener(model)
 
     def is_silence(self, chunk):
         # return True or False
         prob = self.vad.get_prediction(chunk)
+        if self.debug:
+            LOG.debug(f"speech probability: {prob}")
         return prob < self.vad_threshold
