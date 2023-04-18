bbe91116e2bea1d7515a699719b63894583d9bef
JarbasAi <jarbasai@mailfence.com>
2023-04-18 16:44:39 +0100

    fix model, uploaded wrong file

--- ovos_vad_plugin_precise/__init__.py
+++ ovos_vad_plugin_precise/__init__.py
@@ -8,7 +8,7 @@ class PreciseVAD(VADEngine):
     def __init__(self, config=None, sample_rate=None):
         super().__init__(config, sample_rate)
         model = self.config.get("model") or join(dirname(__file__), "vad.tflite")
-        self.vad_threshold = self.config.get("threshold", 0.5)
+        self.vad_threshold = self.config.get("threshold", 0.35)
         self.debug = self.config.get("debug", False)
         self.vad = Listener(model)
 
